ddf859160e4aab0e7fd2fe209ebe25c0b5575c50
arijit <arijitmail.com>
2022-06-13 19:15:37 +0530

    argha johjdow

--- pages/argha.js
+++ pages/argha.js
@@ -1,7 +1,19 @@
+import { useRouter } from "next/router"
 import React from "react"
 
-const argha = () => {
-  return <div>God level coder</div>
+const Argha = () => {
+  const router = useRouter()
+  const apiCall = () => {
+    fetch("http://localhost:3000/api/hello")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+        router.push(`/${data.name.split(" ").join("").toLowerCase()}`)
+      })
+      .catch((err) => console.log(err))
+  }
+
+  return <button onClick={apiCall}>God level coder</button>
 }
 
-export default argha
+export default Argha
---
+++ pages/johndoe.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const johndoe = () => {
+  return <div>johndoe</div>
+}
+
+export default johndoe
